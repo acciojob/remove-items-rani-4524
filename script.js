92dc34a2bc910f3a4ebfdcbda74469419df4ca7d
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-10 15:56:15 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// const selectElement=document.getElementById("colorSelect");
+// const selectedIndex = selectElement.selectedIndex;
+
+// if (selectedIndex !== -1) {
+//   selectElement.remove(selectedIndex);
+// }
+
+
+const selectElement = document.getElementById("colorSelect");
+
+for (let i = 0; i < selectElement.options.length; i++) {
+  if (selectElement.options[i].text === "Green") {
+    selectElement.remove(i);
+    break;
+  }
+}
